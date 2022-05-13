601acea31aface609db4c8c2f7a3cb803d4ee83b
Rafał Nowak <rwojciechnowak@gmail.com>
2022-05-13 11:50:20 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = 4, liczba_pomocnicza = 1;
+
+            for (int wiersz = 1; wiersz <= liczba; wiersz++)
+            {
+                for (int kolumna = 1; kolumna <= wiersz; kolumna++)
+                {
+                    Console.Write(" " + liczba_pomocnicza);
+                    liczba_pomocnicza++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
